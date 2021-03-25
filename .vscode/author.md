171690902bca61809b8a3efb0fb1dabf3bb9cab8
Sam Sweigart <samuel.sweigart@gmail.com>
2021-03-25 16:07:22 -0400

    switched for loop with forEach() & added author.md

---
+++ .vscode/author.md
@@ -0,0 +1,2 @@
+# *** AUTHOR ***
+# Student in Penn LPS FSF Program: <br/> All work original by: Sam Sweigart
--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -3,26 +3,27 @@ let buttonEl = document.getElementById('start-quiz');
 let mainEl = document.getElementById('switch');
 let h1El = document.getElementById('question');
 let paraEl = document.getElementById('welcome');
+let mChoiceBtn = document.getElementsByClassName('m-choice-btn');
 
 let questions = [
     {
         title: 'Which is a Boolean Value?',
-        answers: ['Hello', 'False', '36', 'false'],
+        responses: ['Hello', 'False', '36', 'false'],
         correct: 'false'
     },
     {
         title: 'What does the DRY principle stand for?',
-        answers: ['Do Rocks Yawn', 'Don\'t Repeat Yourself', 'Don\'t Raise Yeti\'s', 'Definitely Raise Yeti\'s'],
+        responses: ['Do Rocks Yawn', 'Don\'t Repeat Yourself', 'Don\'t Raise Yeti\'s', 'Definitely Raise Yeti\'s'],
         correct: 'Don\'t Repeat Yourself',
     },
     {
         title: 'Which is a String Value?',
-        answers: ["'5'", '5' , 'true', 'NaN'],
+        responses: ["'5'", '5' , 'true', 'NaN'],
         correct: "'5'",
     },
     {
         title: 'Which is an Array Method?',
-        answers: ['makeMyCodeWork()','reduce()', 'toLowerCase()', 'codeMagic()'],
+        responses: ['makeMyCodeWork()','reduce()', 'toLowerCase()', 'codeMagic()'],
         correct: 'reduce()'
     }
 ];
@@ -35,41 +36,40 @@ let score = timer;
 buttonEl.addEventListener('click', () => {
     buttonEl.classList.add('hide');
     paraEl.classList.add('hide')
+
     setInterval(() => {
         timerEl.innerHTML = `Time: ${score}`;
-        if (score > 0) {
-            return score--;
-        }
+        score > 0 ? score-- : score;
     }, 1000);
 
     let question = questions[0];
-    console.log(question)
-
-    for (let i = 0; i < questions.length; i++) {
-        console.log(question.answers[0])
-        let answers = question.answers[i];
+    h1El.textContent= question.title;
+    question.responses.forEach((choice, i) => {
         let li = document.createElement('li');
+        li.setAttribute('class', 'choices');
+        li.textContent = choice;
+        console.log(choice)
+        console.log(i)
+        li.addEventListener('click', (event) => {
+            event.target.innerHTML === questions[0].correct ?
+                score : score -= 10;
+                console.log(score);
+        })
+        document.querySelector('#m-choice-list').appendChild(li);
+    })
+    /* for (let i = 0; i < question.responses.length; i++) {
+        let responses = question.responses[i];
+        let li = document.createElement('li');
+        li.setAttribute('class', 'responses');
+        li.textContent = responses;
+        li.addEventListener('click', (event) => {
+            event.target.innerHTML === questions[0].correct ?
+                score : score -= 10;
+                console.log(score);
 
-        h1El.textContent= question.title;
-        li.textContent = answers;
-
-        document.querySelector('#m-choice').appendChild(li);
-    }
-    
+        }) */
+/*         document.querySelector('#m-choice-list').appendChild(li);
+ */    
 });
-/* 
-    questions.forEach((question, i) => {
-        h1El.textContent= question.title;
-        let answers = question.answers[i];
-        console.log(question)
-        if (i <= answers.length) {
-            i++;
-            let li = document.createElement('li');
-            console.log(answers)
-            li.textContent = answers;
-            
-            document.querySelector('#m-choice').appendChild(li);
-        }
-    }) 
-    */
+
 
