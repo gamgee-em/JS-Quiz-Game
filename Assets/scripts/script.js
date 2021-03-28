ab84a7d3560a183b469628877d3d0e618f64da56
Sam Sweigart <samuel.sweigart@gmail.com>
2021-03-28 08:54:18 -0400

    fixed onclick event. All m-choice questions/responses are now functioning correctly

--- Assets/scripts/script.js
+++ Assets/scripts/script.js
@@ -1,9 +1,9 @@
 let timerEl = document.getElementById('timer');
 let buttonEl = document.getElementById('start-quiz');
-let h1El = document.getElementById('question');
-let qEl = document.getElementById('questions');
-let questIndex = 0;
+let h1El = document.getElementById('responses');
+let questionEl = document.getElementById('questions');
 let mChoiceLi = document.querySelector('#m-choice-list');
+let questIndex = 0;
 
 let questions = [
     {
@@ -18,7 +18,7 @@ let questions = [
     },
     {
         title: 'Which is a String data type?',
-        responses: ['"5"', 5 , 'true', 'NaN'],
+        responses: ["'5'", 5 , 'true', 'NaN'],
         correct: "'5'",
     },
     {
@@ -33,59 +33,62 @@ let score = timer;
 // Add click event to start quiz button
 function startQuiz() {
     let paraEl = document.getElementById('welcome');
-
-/*     event.stopPropagation();
- */
-    console.log('clicked');
+    console.log('clicked start quiz');
     paraEl.setAttribute('class', 'hide');
-    qEl.removeAttribute('class');
+    questionEl.removeAttribute('class');
     changeQuestion();
 
-    /* setInterval(() => {
+    setInterval(() => {
         timerEl.innerHTML = `Time: ${score}`;
         score > 0 ? score-- : score = 0;
-    }, 1000); */
+    }, 1000);
 };
 
 function changeQuestion() {
 
-    let question = questions[questIndex];
-    // Loop over each questions objects choice in the responses array
-    /* console.log(question.responses) */
-/*     for (let i = 0; i < question.responses.length; i++) {
-        h1El.textContent= question.title;
-        let responses = question.responses[i];
-        console.log('1:', responses)
-        let title = questions[i].title
-        console.log('2:', title)
-        let li = document.createElement('li');
-        li.setAttribute("class", "responses")
-        li.textContent =  responses;
- */     
-        h1El.textContent= question.title;
-        mChoiceLi.innerHTML = "";
-     
-        question.responses.forEach(function (response, i) {
-            let button = document.createElement('button');
-            button.setAttribute('class', 'response');
-            button.setAttribute('value', response);
-            button.textContent = i + 1 + '. ' + response;
-            button.onclick = clickQuestion;
-            mChoiceLi.appendChild(button);
+    let question = questions[questIndex];    
+    h1El.textContent= question.title;
+    mChoiceLi.innerHTML = "";
+    console.log(questions[questIndex].correct)
 
-        });
-    }
+    question.responses.forEach(function (response, i) {
+        let button = document.createElement('button');
 
-        function clickQuestion() {
-/*             let question = questions[questIndex];
- */            /* h1El.textContent = title; */
+        button.setAttribute('class', 'response');
+        button.setAttribute('value', response);
+        button.textContent = i + 1 + '. ' + response;
+        button.onclick = clickQuestion;
+        mChoiceLi.appendChild(button);
+
+    });
+}
+    
+function clickQuestion() {
+    this.value === questions[questIndex].correct ?
+        score : score -= 10;
+
+    questIndex++;
+    if (questIndex === questions.length) {
+        // end quiz function here
+        console.log('Game Over!');
+        alert(`Your score is ${score}`)
+    } 
+            // Else run this changeQuestion function
+    changeQuestion();
+    console.log('clicked a response');
+
+}
+buttonEl.onclick = startQuiz;
+/*         function clickQuestion() {
+            let question = questions[questIndex];
+            h1El.textContent = title; 
             questIndex++;
-            console.log('click');
-    /*     button.addEventListener("click", function(){ */
+            console.log('click'); 
+        button.addEventListener("click", function(){ 
 
-/*             console.log(`user chose = ${responses}`)
- */
-            /* this.innerHTML === question.correct ?
+           console.log(`user chose = ${responses}`)
+ 
+             this.innerHTML === question.correct ?
                 score & console.log('correct!') :
                 score -= 10;
 
@@ -95,12 +98,11 @@ function changeQuestion() {
             // this value needs to be something different
             console.log(question.responses[questIndex])
             button.innerHTML = question.responses[questIndex];
-            changeQuestion(); */
-/*             questIndex++;
- */ 
-/*         })
- */    }
-/*         mChoiceLi.appendChild(li);
- */    /* } */
+            changeQuestion(); 
+             questIndex++;
+  
+         })
+      } 
+         mChoiceLi.appendChild(li);
+  } */
 
-buttonEl.onclick = startQuiz;
